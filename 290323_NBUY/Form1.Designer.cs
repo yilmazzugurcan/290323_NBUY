de9e4ba966214d3d565e803a3634648becdc4419
yilma <yilma@DESKTOP-U1LH654>
2023-03-29 18:02:49 +0300

    Add project files.

---
+++ 290323_NBUY/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace _290323_NBUY
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtad = new System.Windows.Forms.TextBox();
+            this.txtsoyad = new System.Windows.Forms.TextBox();
+            this.txtTc = new System.Windows.Forms.TextBox();
+            this.txtMeslek = new System.Windows.Forms.TextBox();
+            this.txtTlf = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnekle = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.listBox5 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.listBox6 = new System.Windows.Forms.ListBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtad
+            // 
+            this.txtad.Location = new System.Drawing.Point(70, 29);
+            this.txtad.Name = "txtad";
+            this.txtad.Size = new System.Drawing.Size(100, 20);
+            this.txtad.TabIndex = 0;
+            this.txtad.Text = "Ad Girin";
+            this.txtad.TextChanged += new System.EventHandler(this.txtad_Enter);
+            this.txtad.Enter += new System.EventHandler(this.txtad_Enter);
+            // 
+            // txtsoyad
+            // 
+            this.txtsoyad.Location = new System.Drawing.Point(70, 70);
+            this.txtsoyad.Name = "txtsoyad";
+            this.txtsoyad.Size = new System.Drawing.Size(100, 20);
+            this.txtsoyad.TabIndex = 1;
+            this.txtsoyad.Text = "Soyad Girin";
+            this.txtsoyad.Enter += new System.EventHandler(this.txtsoyad_Enter);
+            // 
+            // txtTc
+            // 
+            this.txtTc.Location = new System.Drawing.Point(207, 29);
+            this.txtTc.Name = "txtTc";
+            this.txtTc.Size = new System.Drawing.Size(100, 20);
+            this.txtTc.TabIndex = 2;
+            this.txtTc.Text = "TC Girin";
+            this.txtTc.Enter += new System.EventHandler(this.txtTc_Enter);
+            // 
+            // txtMeslek
+            // 
+            this.txtMeslek.Location = new System.Drawing.Point(207, 71);
+            this.txtMeslek.Name = "txtMeslek";
+            this.txtMeslek.Size = new System.Drawing.Size(100, 20);
+            this.txtMeslek.TabIndex = 3;
+            this.txtMeslek.Text = "Meslek Girin";
+            this.txtMeslek.Enter += new System.EventHandler(this.txtMeslek_Enter);
+            // 
+            // txtTlf
+            // 
+            this.txtTlf.Location = new System.Drawing.Point(356, 29);
+            this.txtTlf.Name = "txtTlf";
+            this.txtTlf.Size = new System.Drawing.Size(100, 20);
+            this.txtTlf.TabIndex = 4;
+            this.txtTlf.Text = "Telefon No Girin";
+            this.txtTlf.Enter += new System.EventHandler(this.txtTlf_Enter);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Kadın",
+            "Erkek"});
+            this.comboBox1.Location = new System.Drawing.Point(356, 71);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(100, 21);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // btnekle
+            // 
+            this.btnekle.Location = new System.Drawing.Point(632, 29);
+            this.btnekle.Name = "btnekle";
+            this.btnekle.Size = new System.Drawing.Size(75, 23);
+            this.btnekle.TabIndex = 6;
+            this.btnekle.Text = "Ekle";
+            this.btnekle.UseVisualStyleBackColor = true;
+            this.btnekle.Click += new System.EventHandler(this.btnekle_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(713, 29);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Temizle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 137);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(100, 212);
+            this.listBox1.TabIndex = 8;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(153, 137);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(100, 212);
+            this.listBox2.TabIndex = 9;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(278, 135);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(100, 212);
+            this.listBox3.TabIndex = 10;
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.Location = new System.Drawing.Point(525, 135);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(100, 212);
+            this.listBox4.TabIndex = 11;
+            // 
+            // listBox5
+            // 
+            this.listBox5.FormattingEnabled = true;
+            this.listBox5.Location = new System.Drawing.Point(658, 137);
+            this.listBox5.Name = "listBox5";
+            this.listBox5.Size = new System.Drawing.Size(100, 212);
+            this.listBox5.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 119);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "AD";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(326, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "TC";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(545, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "TELEFON";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(678, 119);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "CİNSİYET";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(179, 119);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "SOYAD";
+            // 
+            // listBox6
+            // 
+            this.listBox6.FormattingEnabled = true;
+            this.listBox6.Location = new System.Drawing.Point(405, 135);
+            this.listBox6.Name = "listBox6";
+            this.listBox6.Size = new System.Drawing.Size(99, 212);
+            this.listBox6.TabIndex = 18;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(430, 119);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 13);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "MESLEK";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.listBox6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox5);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnekle);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.txtTlf);
+            this.Controls.Add(this.txtMeslek);
+            this.Controls.Add(this.txtTc);
+            this.Controls.Add(this.txtsoyad);
+            this.Controls.Add(this.txtad);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtad;
+        private System.Windows.Forms.TextBox txtsoyad;
+        private System.Windows.Forms.TextBox txtTc;
+        private System.Windows.Forms.TextBox txtMeslek;
+        private System.Windows.Forms.TextBox txtTlf;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnekle;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.ListBox listBox5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox listBox6;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ 290323_NBUY/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _290323_NBUY
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void btnekle_Click(object sender, EventArgs e)
+        {
+            string ad = txtad.Text;
+            listBox1.Items.Add(ad);
+            txtad.Text = "Ad Girin";
+            string soyad = txtsoyad.Text;
+            listBox2.Items.Add(soyad);
+            txtsoyad.Text = "Soyad Girin";
+            string tc = txtTc.Text;
+            listBox3.Items.Add(tc);
+            txtTc.Text = "Tc Girin";
+            string meslek = txtMeslek.Text;
+            listBox6.Items.Add(meslek);
+            txtMeslek.Text = "Meslek Girin";
+            string tlf = txtTlf.Text;
+            listBox4.Items.Add(tlf);
+            txtTlf.Text = "Telefon Girin";
+            string cinsiyet = comboBox1.SelectedItem.ToString();
+            listBox5.Items.Add(cinsiyet);
+            comboBox1.Items.Remove(cinsiyet);
+
+
+        }
+
+        private void txtad_Enter(object sender, EventArgs e)
+        {
+          //  txtad.Text = string.Empty;
+        }
+
+        private void txtsoyad_Enter(object sender, EventArgs e)
+        {
+          //  txtsoyad.Text = string.Empty;
+        }
+
+        private void txtTc_Enter(object sender, EventArgs e)
+        {
+           // txtTc.Text=string.Empty;
+        }
+
+        private void txtMeslek_Enter(object sender, EventArgs e)
+        {
+           // txtMeslek.Text = string.Empty;
+        }
+
+        private void txtTlf_Enter(object sender, EventArgs e)
+        {
+          //  txtTlf.Text = string.Empty;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            listBox3.Items.Clear();
+            listBox4.Items.Clear();
+            listBox5.Items.Clear();
+            listBox6.Items.Clear();
+        }
+    }
+}
